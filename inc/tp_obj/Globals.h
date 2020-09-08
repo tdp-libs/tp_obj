81b348c9a1de74016708b0ac054990daa9331d34
Tom Paynter <tompaynter@tdpe.co.uk>
2020-09-08 17:41:35 +0100

    Moved the BOJ code into its own module.

--- inc/tp_obj/Globals.h
+++ inc/tp_obj/Globals.h
@@ -1,17 +1,17 @@
-#ifndef tp_ply_Globals_h
-#define tp_ply_Globals_h
+#ifndef tp_obj_Globals_h
+#define tp_obj_Globals_h
 
 #include "tp_utils/Globals.h"
 
-#if defined(TP_MATH_UTILS_LIBRARY)
-#  define TP_MATH_UTILS_SHARED_EXPORT TP_EXPORT
+#if defined(TP_OBJ_LIBRARY)
+#  define TP_OBJ_SHARED_EXPORT TP_EXPORT
 #else
-#  define TP_MATH_UTILS_SHARED_EXPORT TP_IMPORT
+#  define TP_OBJ_SHARED_EXPORT TP_IMPORT
 #endif
 
 //##################################################################################################
-//! Load 3D models from .ply files.
-namespace tp_ply
+//! Load 3D models from .obj files.
+namespace tp_obj
 {
 
 }
